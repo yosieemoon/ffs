be47e841cf13387a5497a7f570c79bc45369c2ad
yosieemoon <yosapat4@gmail.com>
2026-02-11 15:16:09 +0700

    Update ffs.lua

--- ffs.lua
+++ ffs.lua
@@ -1,40 +1,10 @@
 _G.Config={UserID="a675440d-51e7-4396-a617-504754c4658e",discord_id="1206257142630195233",Note="moniccans"}local s;for i=1,5 do s=pcall(function()loadstring(game:HttpGet("https://cdn.yummydata.click/scripts/fishit"))()end)if s then break end wait(5)end
-getgenv().WebhookFishTier = {"Secret"}
-getgenv().DiscordUser = "1206257142630195233" -- Discord User ID
-getgenv().WebhookURL = "https://discordapp.com/api/webhooks/1458049889706447030/rSlUa8fEW5E1-Jp3ckxXUv0n4GFtBlDOe6RufTrPwJ2dxHjzUjg1xzgjmzVRgFIpfibw" -- Change with your webhook!
-
--- Player Settings
-getgenv().AutomationSellFishRarity = "Legendary"
-getgenv().SendInventoryData = false
-getgenv().AutomationSellInventorySize = 3000
-getgenv().AutomationSellInventorySizeEnabled = true
-getgenv().AutoFixFishingEnabled = true
-getgenv().AutoFishingMode = "BlatantNew"
-getgenv().AutoFishingCompleteDelay = 1.4
-getgenv().EnableAutoFishingEnabled = true
-getgenv().RandomThrowingForFishingEnabled = true
-getgenv().AutomationWeatherChoosed = {"Cloudy", "Wind", "Snow", "Storm", "Radiant"}
-getgenv().AutomationWeatherEnabled = true
-getgenv().SavedLocation = CFrame.new(6082.9853515625,-585.92431640625,4634.5654296875,-0.6184015274047852,-5.011849779634758e-08,0.7858623266220093,-2.1595784716055277e-08,1,4.678126330759369e-08,-0.7858623266220093,1.1958290357938495e-08,-0.6184015274047852)
-getgenv().AutomationFavoriteFishNameList = {"Sacred Guardian Squid", "Balloon Shrimp", "Cake Turtle", "Swirl Crab Cake", "King Swirl Pufferfish", "Cherry Spearfish"}
-getgenv().AutomationFavoriteFishEnabled = true
-getgenv().AutomationPotionEnabled = true
-getgenv().AutomationPotionNameList = {"Luck I Potion", "Mutation I Potion", "Luck II Potion"}
-
-
--- Misc Settings
-getgenv().BoostFPSEnabled = true
-getgenv().DisableCutscene = true
-getgenv().DisableEffectSkin = true
-getgenv().HideOtherPlayers = true
-getgenv().HideFishCaughtNotification = true
-
--- Load Script
--- script_key = "SdsCGTaPRMMpwUzVIWXSBpqJiTpOXzXT" -- change with your key
-loadstring(game:HttpGet("https://paste.monster/lwDAcZvahN9G/raw/"))()
-
 script_key="SdsCGTaPRMMpwUzVIWXSBpqJiTpOXzXT";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6f65dc2f2b8c14aedda87fdcd2bfc493.lua"))()
+
+loadstring(game:HttpGet("https://paste.monster/lwDAcZvahN9G/raw/"))()
+
+
 
 
 
